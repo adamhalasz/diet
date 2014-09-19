b556289ccbb4f612ecd006254a21059e042773ef
Adam Halasz <adamfsh@gmail.com>
2014-09-20 01:14:25 +0200

    v0.8.0

--- tests/repl.js
+++ tests/repl.js
@@ -9,30 +9,10 @@ var repl = require('repl');
 describe(subject + 'start a diet app from the command line', function(){	
 	it('app.get(\'/\', ..)'.white+' - imitate repl call'.grey
 	, function(done){
-		var server = require('diet');
+		var server = require('../');
 		var app = new server({ _repl: true });
 		app.domain(9040);
 		app.get('/', function($){ $.end('hello') });
 		done();
 	});
 });
-
-/*
-/*
-describe(subject + 'start a diet app from the command line', function(){	
-	it('app.get(\'/\', ..)'.white+' - create an app from the command line'.grey
-	, function(done){
-		
-		repl.start({
-		  prompt: 'node',
-		  input: process.stdin,
-		  terminal:true,
-		  output: process.stdout
-		});
-		process.stdout.write('require('diet').hey()');
-		done()
-	});
-});
-
-*/
-	
