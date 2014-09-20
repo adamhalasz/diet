8f123c35104ae4dc2ff17c0b01119b15402c07d3
Adam Halasz <adamfsh@gmail.com>
2014-09-20 06:26:54 +0200

    v0.8.1
    + added `app.server`
    + cleaned up unnecessary console.logs

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 var platform = module.platform = process.platform;
-console.log(module.platform);
-
 module.exports = require('./lib/index.js');
-
 module.exports.server = function(location){
-	console.log(platform);
 	var server = require('./lib/index.js');
 	var app = new server({ platform: platform });
 	if(location) app.domain(location);
